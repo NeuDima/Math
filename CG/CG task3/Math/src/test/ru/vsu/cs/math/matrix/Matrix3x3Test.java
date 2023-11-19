a92896aa261222966706c3540aaa208d8867ecbc
Dmitry <dimok.neupokoev@gmail.com>
2023-11-19 22:08:57 +0300

    Library for working with vectors 2f, 3f, 4f and matrices 3x3, 4x4

--- CG/CG task3/Math/src/test/ru/vsu/cs/math/matrix/Matrix3x3Test.java
+++ CG/CG task3/Math/src/test/ru/vsu/cs/math/matrix/Matrix3x3Test.java
@@ -8,48 +8,84 @@ import org.junit.jupiter.api.Test;
 public class Matrix3x3Test {
     @Test
     void zeroMatrix() {
-        Matrix3x3 matrix1 = new Matrix3x3(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        Matrix3x3 matrix1 = new Matrix3x3(
+                0, 0, 0,
+                0, 0, 0,
+                0, 0, 0);
         Matrix3x3 matrix2 = new Matrix3x3();
         Assertions.assertTrue(matrix1.equals(matrix2));
     }
 
     @Test
     void identityMatrix() {
-        Matrix3x3 matrix1 = new Matrix3x3(1, 0, 0, 0, 1, 0, 0, 0, 1);
+        Matrix3x3 matrix1 = new Matrix3x3(
+                1, 0, 0,
+                0, 1, 0,
+                0, 0, 1);
         Matrix3x3 matrix2 = new Matrix3x3().identityMatrix();
         Assertions.assertTrue(matrix1.equals(matrix2));
     }
 
     @Test
     void add() {
-        Matrix3x3 matrix1 = new Matrix3x3(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        Matrix3x3 matrix2 = new Matrix3x3(4, 5, 3, 1, 4, 5, 6, 9, 0);
+        Matrix3x3 matrix1 = new Matrix3x3(
+                1, 2, 3,
+                4, 5, 6,
+                7, 8, 9);
+        Matrix3x3 matrix2 = new Matrix3x3(
+                4, 5, 3,
+                1, 4, 5,
+                6, 9, 0);
         Matrix3x3 matrixResult = matrix1.add(matrix2);
-        Matrix3x3 matrix = new Matrix3x3(5, 7, 6, 5, 9, 11, 13, 17, 9);
+        Matrix3x3 matrix = new Matrix3x3(
+                5, 7, 6,
+                5, 9, 11,
+                13, 17, 9);
         Assertions.assertTrue(matrix.equals(matrixResult));
     }
 
     @Test
     void sub() {
-        Matrix3x3 matrix1 = new Matrix3x3(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        Matrix3x3 matrix2 = new Matrix3x3(4, 5, 3, 1, 4, 5, 6, 9, 0);
+        Matrix3x3 matrix1 = new Matrix3x3(
+                1, 2, 3,
+                4, 5, 6,
+                7, 8, 9);
+        Matrix3x3 matrix2 = new Matrix3x3(
+                4, 5, 3,
+                1, 4, 5,
+                6, 9, 0);
         Matrix3x3 matrixResult = matrix1.sub(matrix2);
-        Matrix3x3 matrix = new Matrix3x3(-3, -3, 0, 3, 1, 1, 1, -1, 9);
+        Matrix3x3 matrix = new Matrix3x3(
+                -3, -3, 0,
+                3, 1, 1,
+                1, -1, 9);
         Assertions.assertTrue(matrix.equals(matrixResult));
     }
 
     @Test
     void mul() {
-        Matrix3x3 matrix1 = new Matrix3x3(1, 4, 3, 2, 1, 5, 3, 2, 1);
-        Matrix3x3 matrix2 = new Matrix3x3(5, 2, 1, 4, 3, 2, 2, 1, 5);
+        Matrix3x3 matrix1 = new Matrix3x3(
+                1, 4, 3,
+                2, 1, 5,
+                3, 2, 1);
+        Matrix3x3 matrix2 = new Matrix3x3(
+                5, 2, 1,
+                4, 3, 2,
+                2, 1, 5);
         Matrix3x3 matrixResult = matrix2.mulMatrix(matrix1);
-        Matrix3x3 matrix = new Matrix3x3(12, 24, 26, 16, 23, 29, 19, 19, 16);
+        Matrix3x3 matrix = new Matrix3x3(
+                12, 24, 26,
+                16, 23, 29,
+                19, 19, 16);
         Assertions.assertTrue(matrix.equals(matrixResult));
     }
 
     @Test
     void mulVector() {
-        Matrix3x3 matrix1 = new Matrix3x3(2, 4, 0, -2, 1, 3, -1, 0, 1);
+        Matrix3x3 matrix1 = new Matrix3x3(
+                2, 4, 0,
+                -2, 1, 3,
+                -1, 0, 1);
         Vector3f vector3f = new Vector3f(1, 2, -1);
         Vector3f matrixResult = matrix1.mulVector(vector3f);
         Vector3f matrix = new Vector3f(10, -3, -2);
@@ -58,21 +94,36 @@ public class Matrix3x3Test {
 
     @Test
     void determinant() {
-        Matrix3x3 matrix1 = new Matrix3x3(1, 4, 3, 2, 1, 5, 3, 2, 1);
+        Matrix3x3 matrix1 = new Matrix3x3(
+                1, 4, 3,
+                2, 1, 5,
+                3, 2, 1);
         Assertions.assertEquals(46, matrix1.determinantMatrix());
     }
 
     @Test
     void transposition() {
-        Matrix3x3 matrix1 = new Matrix3x3(1, 4, 3, 2, 1, 5, 3, 2, 1);
-        Matrix3x3 matrixTr = new Matrix3x3(1, 2, 3, 4, 1, 2, 3, 5, 1);
+        Matrix3x3 matrix1 = new Matrix3x3(
+                1, 4, 3,
+                2, 1, 5,
+                3, 2, 1);
+        Matrix3x3 matrixTr = new Matrix3x3(
+                1, 2, 3,
+                4, 1, 2,
+                3, 5, 1);
         Assertions.assertTrue(matrixTr.equals(matrix1.transposition()));
     }
 
     @Test
     void inverseMatrix() {
-        Matrix3x3 matrix1 = new Matrix3x3(2, 5, 7, 6, 3, 4, 5, -2, -3);
-        Matrix3x3 matrixIn = new Matrix3x3(1, -1, 1, -38, 41, -34, 27, -29, 24);
+        Matrix3x3 matrix1 = new Matrix3x3(
+                2, 5, 7,
+                6, 3, 4,
+                5, -2, -3);
+        Matrix3x3 matrixIn = new Matrix3x3(
+                1, -1, 1,
+                -38, 41, -34,
+                27, -29, 24);
         Assertions.assertTrue(matrixIn.equals(matrix1.inverseMatrix()));
     }
 
